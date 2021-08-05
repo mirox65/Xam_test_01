3ffb26aea972da95ac111643ae49c7a64ab2536a
mirox65 <39920445+mirox65@users.noreply.github.com>
2021-08-05 14:38:40 +0200

    Uspješno integriran test

---
+++ FizikaKpk_UnitTest/FormuleTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xam_test_01.Pomocne;
+
+namespace FizikaKpk_UnitTest
+{
+    [TestClass]
+    public class FormuleTests
+    {
+        private readonly double brzina = 7.42;
+        private readonly double vrijeme = 4.15;
+        private readonly double put = 30.79;
+
+        [TestMethod]
+        public void SvtFormula_Izračun_PovratDoubleVrijednost()
+        {
+            var rezultat = Formule.SvtFormula(brzina, vrijeme);
+
+            Assert.AreEqual(put, rezultat);
+        }
+
+        [TestMethod]
+        public void VstFormula_Izračun_PovratDoubleVrijednost()
+        {
+            var rezultat = Formule.VstFormula(put, vrijeme);
+
+            Assert.AreEqual(brzina, rezultat);
+        }
+
+        [TestMethod]
+        public void TsvFormula_Izračun_PovratDoubleVrijednost()
+        {
+            var rezultat = Formule.TsvFormula(put, brzina);
+
+            Assert.AreEqual(vrijeme, rezultat);
+        }
+    }
+}
